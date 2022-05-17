e461f244793b79bb05ab67d7456089a47bd99b06
Mahmut Orak <mahmut.orak@bentego.com>
2022-05-17 09:47:50 +0300

    *sleep times

--- backend-spring-boot-crud-full-stack/src/main/java/com/openvpn/service/Exepct4J.java
+++ backend-spring-boot-crud-full-stack/src/main/java/com/openvpn/service/Exepct4J.java
@@ -15,48 +15,40 @@ public class Exepct4J {
             ExpectJ expectinator = new ExpectJ(2);
 
             // Fork the process
-            // Spawn shell = expectinator.spawn("/bin/bash");
+            Spawn shell = expectinator.spawn("/home/bentegopersonel/workspace2/openvpn_gui/backend-spring-boot-crud-full-stack/ovn.sh\n");
 
-            Spawn shell = expectinator.spawn("/home/bentegopersonel/workspace2/backend-spring-boot-crud-full-stack/ovn.sh\n");
-            String stdOUt  = shell.getCurrentStandardOutContents();
-            String errOut = shell.getCurrentStandardErrContents();
-
-            Thread.sleep(2);
+            Thread.sleep(3);
+            String stdOut = shell.getCurrentStandardOutContents();
+            System.out.println(stdOut);
 
             // Talk to it
-            // shell.send("/home/bentegopersonel/workspace2/backend-spring-boot-crud-full-stack/ovn.sh\n");
-            // String outContents = shell.getCurrentStandardOutContents();
-            // System.out.println("outContents: " + outContents);
-
             shell.send("1\n");
+            Thread.sleep(3);
+            stdOut = shell.getCurrentStandardOutContents();
+            System.out.println(stdOut);
 
             shell.send("myclient\n");
+            Thread.sleep(3);
+            stdOut = shell.getCurrentStandardOutContents();
+            System.out.println(stdOut);
+
+            shell.send("243\n");
+            Thread.sleep(3);
+            stdOut = shell.getCurrentStandardOutContents();
+            System.out.println(stdOut);
 
             // send ctrl + c
             shell.send("0x03\n");
-
-
             Thread.sleep(3);
+            stdOut = shell.getCurrentStandardOutContents();
+            System.out.println(stdOut);
 
-            shell.send("exit\n");
             shell.stop();
 
 
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-
-
-//        shell.send("\\010\n");
-//
-//        shell.send("1\n");
-//
-//        shell.send("exit\n");
-
-//        shell.expectClose();
-
-
-
     }
 
 }
